0b3972b080127690f9f9432348d450b9768f3f29
Tommy Miland <kontakt@tmiland.com>
2025-06-20 14:41:56 +0200

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-arch=`uname -m`
+
+VERSION=$(curl -s https://icecatbrowser.org/all_downloads.html |
+  grep -Po 'b>\K.*(?=</b)' |
+  head -n 1 |
+  sed -n 's/[^0-9.]*\([0-9.]*\).*/\1/p' |
+  # Stript trailing .
+sed 's/.$//')
+# Replace Version in ./amd64/DEBIAN/control file
+sed -i "s/Version: .*/Version: $VERSION/" ./amd64/DEBIAN/*
+
+arch=$(uname -m)
 if [ "$arch" == "x86_64" ]; then
-    arch="amd64"
+  arch="amd64"
 else
-    arch="x86"
+  arch="x86"
+  echo "Error: $arch is no longer supported..."
+  exit 1
 fi
 
 dpkg-deb --build $arch gnu-icecat_$arch.deb
